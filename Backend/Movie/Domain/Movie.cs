0c5ec05e3dcc531993752adba833396086524125
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-04-12 16:38:00 +0200

    feat(5,6): Added endpoint to search for movie by title. And connected to TMDB to query with specified title
    Co-authored-by: Bimmerlynge <mathiashome@hotmail.com>

---
+++ Backend/Movie/Domain/Movie.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Movie.Domain;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public int Id { get; set; }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,8 +1,15 @@
+using Backend.Movie.Infrastructure;
+using Backend.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IMovieService, TMDBService>();
+builder.Services.AddSingleton<IMovieRepository, MovieRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
