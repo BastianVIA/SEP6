c736adaec0b35926f4895c891173786c61dbcae8
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-04-19 11:42:53 +0200

    feat(4,5,6): Refactor to now use local database
    Co-authored-by: Bimmerlynge <mathiashome@hotmail.com>

---
+++ Backend/Movie/Infrastructure/MovieDAO.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Movie.Infrastructure;
+
+public class MovieDAO
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+}
--- Backend/Movie/Infrastructure/MovieRepository.cs
+++ Backend/Movie/Infrastructure/MovieRepository.cs
@@ -1,35 +1,43 @@
-﻿using Backend.Service;
+﻿using Backend.Database;
+using Backend.Service;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Movie.Infrastructure;
 
 public class MovieRepository : IMovieRepository
 {
-    private IMovieService _movieService;
+    private DataContext _database;
 
-    public MovieRepository(IMovieService movieService)
+    public MovieRepository(DataContext database)
     {
-        _movieService = movieService;
+        _database = database;
     }
 
     public async Task<List<Domain.Movie>> SearchForMovie(string title)
     {
-        var moviesFound = await _movieService.SearchForMovie(title);
+        var foundMovies = _database.Movies.Where(m => m.Title.Contains(title)).ToList();
 
-        return ToDomain(moviesFound);
+        return ToDomain(foundMovies);
     }
 
-    private List<Domain.Movie> ToDomain(List<ServiceMovie> serviceMovies)
+    private List<Domain.Movie> ToDomain(List<MovieDAO> movieDaos)
     {
-        var domainMovies = new List<Domain.Movie>();
-        foreach (var serviceMovie in serviceMovies)
+        var listOfDomainMovies = new List<Domain.Movie>();
+        foreach (var movieDao in movieDaos)
         {
-            domainMovies.Add(new Domain.Movie
-            {
-                Id = serviceMovie.Id,
-                ReleaseYear = serviceMovie.ReleaseYear,
-                Title = serviceMovie.Title
-            });
+            listOfDomainMovies.Add(ToDomain(movieDao));
         }
-        return domainMovies;
+
+        return listOfDomainMovies;
+    }
+
+    private Domain.Movie ToDomain(MovieDAO movieDao)
+    {
+        return new Domain.Movie
+        {
+            Id = movieDao.Id,
+            Title = movieDao.Title,
+            ReleaseYear = movieDao.Year
+        };
     }
 }
