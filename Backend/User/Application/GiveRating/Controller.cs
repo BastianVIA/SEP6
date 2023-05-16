f984ee1a7a5114350685ca2463c1c0ee91e594e5
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-05-16 11:12:09 +0200

    feat(60): Schema for set rating

---
+++ Backend/User/Application/GiveRating/Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.User.Application.GiveRating;
+
+[ApiController]
+[Route("user")]
+
+public class Controller : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public Controller(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+
+  
+    [HttpPut]
+    [Route("rating")]
+    [Tags("UserApi")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Authorize]
+    public async Task<IActionResult> Put([FromBody] SetRatingRequest request)
+    {
+
+        return Ok();
+    }
+    
+    public class SetRatingRequest
+    {
+        [Required]
+        public string MovieId { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+    }
+
+}
