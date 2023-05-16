5748e1cffa14c72dbae3778673b9bd2e399e56a0
BastianVIA <305294@VIAUC.dk>
2023-05-16 14:28:25 +0200

    feat(59): Added recommended movies to home page

---
+++ Frontend/Model/Recommendations/RecommendationsModel.cs
@@ -0,0 +1,20 @@
+﻿using Frontend.Entities;
+using Frontend.Network;
+using Frontend.Network.Recommendations;
+
+namespace Frontend.Model.Recommendations;
+
+public class RecommendationsModel : NSwagBaseClient, IRecommendationsModel
+{
+    private IRecommendationsClient _client;
+    
+    public RecommendationsModel(IRecommendationsClient client)
+    {
+        _client = client;
+    }
+    
+    public async Task<List<Movie>> GetRecommendations()
+    {
+        return await _client.GetRecommendations();
+    }
+}
---
+++ Frontend/Network/Recommendations/RecommendationsClient.cs
@@ -0,0 +1,34 @@
+﻿using Frontend.Entities;
+
+namespace Frontend.Network.Recommendations;
+
+public class RecommendationsClient : NSwagBaseClient, IRecommendationsClient
+{
+    public async Task<List<Movie>> GetRecommendations()
+    {
+        var response = await _api.RecommendationsAsync();
+        List<Movie> movies = new List<Movie>();
+        Rating rating = new Rating();
+        foreach (var movie in response.MovieRecommendations)
+        {
+            try
+            {
+                rating = new Rating { AverageRating = movie.Rating.AverageRating, RatingCount = movie.Rating.Votes };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Uri.TryCreate(movie.PathToPoster?.ToString(), UriKind.Absolute, out Uri? posterUri);
+            movies.Add(new Movie
+            {
+                Id = movie.Id, 
+                Title = movie.Title, 
+                ReleaseYear = movie.ReleaseYear, 
+                PosterUrl = posterUri ?? new Uri(DEFAULT_POSTER_URL, UriKind.Relative),
+                Rating = rating
+            });
+        }
+        return movies;
+    }
+}
