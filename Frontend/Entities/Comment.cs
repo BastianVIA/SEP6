7e3d58932833a51b6fa1e1d90fb1deced3aeac5e
BastianVIA <305294@VIAUC.dk>
2023-05-23 16:20:53 +0200

    feat(61): Added functionality to follow users in frontend

--- Frontend/Entities/Comment.cs
+++ Frontend/Entities/Comment.cs
@@ -6,4 +6,6 @@ public class Comment
     public string DisplayNameOfUser { get; set; }
     public string UserId { get; set; }
     public string Content { get; set; }
+    public string? ProfilePicture { get; set; }
+    public DateTimeOffset TimeStamp { get; set; }
 }
