d6552f1a58fdf7a070c588a7961e19a67e878957
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-05-23 02:35:26 +0200

    chore(general): merge with main

--- Backend/Service/IPersonService.cs
+++ Backend/Service/IPersonService.cs
@@ -2,10 +2,10 @@
 
 public interface IPersonService
 {
-    Task<PersonDto?> GetPersonAsync(string id);
+    Task<PersonServiceDto?> GetPersonAsync(string id);
 }
 
-public class PersonDto
+public class PersonServiceDto
 {
     public Uri? PathToProfilePic { get; set; }
     public string? Bio { get; set; }
