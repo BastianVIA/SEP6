5d88f10f2a98c2b57ebe5d1896c5608642e6671a
Nissen99 <47100802+Nissen99@users.noreply.github.com>
2023-05-26 10:25:47 +0200

    fix(general): tests

--- TestBackend/User/Application/SetUserImage/CommandHandlerTests.cs
+++ TestBackend/User/Application/SetUserImage/CommandHandlerTests.cs
@@ -1,6 +1,9 @@
 ﻿using AutoFixture;
+using Backend.Database.Transaction;
+using Backend.Database.TransactionManager;
 using Backend.Service;
 using Backend.User.Application.SetUserImage;
+using Backend.User.Infrastructure;
 using NSubstitute;
 
 namespace TestBackend.User.Application.SetUserImage;
@@ -9,11 +12,12 @@ public class CommandHandlerTests
 {
     private CommandHandler _handler;
     private Fixture _fixture = new();
-    private readonly IUserImageService _userImageService = Substitute.For<IUserImageService>();
+    private readonly IDatabaseTransactionFactory _transactionFactory = Substitute.For<IDatabaseTransactionFactory>();
+    private readonly IUserImageRepository _userImageService = Substitute.For<IUserImageRepository>();
 
     public CommandHandlerTests()
     {
-        _handler = new CommandHandler(_userImageService);
+        _handler = new CommandHandler(_userImageService, _transactionFactory);
     }
 
     [Fact]
@@ -22,6 +26,6 @@ public class CommandHandlerTests
         var command = _fixture.Create<Command>();
 
         await _handler.Handle(command, CancellationToken.None);
-        await _userImageService.Received(1).UploadImageAsync(command.userId, command.data);
+        await _userImageService.Received(1).UploadImageAsync(command.userId, command.data, _fixture.Create<DbTransaction>());
     }
 }
